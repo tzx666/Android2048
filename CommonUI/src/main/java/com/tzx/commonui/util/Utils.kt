90ce734fd837c50a0d515020df9c603b42e1974e
tzx666 <916196773@qq.com>
2021-01-01 19:57:03 +0800

    屏幕计算

--- CommonUI/src/main/java/com/tzx/commonui/util/Utils.kt
+++ CommonUI/src/main/java/com/tzx/commonui/util/Utils.kt
@@ -1,8 +1,15 @@
 package com.tzx.commonui.util
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.util.DisplayMetrics
+import android.util.TypedValue
+
+
+
+
 
 interface Callback{
     fun onConfirm(dialog: DialogInterface);
@@ -30,4 +37,18 @@ public  fun showNormalDialog(context:Context,title: String, msg: String,callback
     normalDialog.setNegativeButton("取消"){ dialog, which -> callback.onCancel(dialog) }
     // 显示
     normalDialog.show()
+}
+public fun getWidth(context: Context):Int{
+    val outMetrics = DisplayMetrics()
+   (context as Activity)?.getWindowManager().getDefaultDisplay().getMetrics(outMetrics)
+    val widthPixels = outMetrics.widthPixels
+    val heightPixels = outMetrics.heightPixels
+    return widthPixels
+}
+fun dp2px(context: Context, dp: Float): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        context.resources.displayMetrics
+    )
 }
